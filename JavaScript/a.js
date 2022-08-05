741dac6826529ea623ee2b4a70aac1875b2f8252
wailinnkyaw03 <102975967+wailinnkyaw03@users.noreply.github.com>
2022-08-05 09:44:13 +0630

    a

---
+++ JavaScript/a.js
@@ -0,0 +1,29 @@
+let a = 5
+let b = 4
+let c = a+b
+console.log(c)
+
+let x = "Hello, "
+let y = " Wai Linn Kyaw!"
+let z = x + y
+console.log(z)
+
+let e = "My name is "
+let f = "Age: "
+let g = "25 years old!"
+console.log(e+y)
+console.log(f+g)
+
+let h = 30
+let i = 20
+if (h<i){
+    console.log("You are amazing!")
+}else{
+    console.log("You are bad!")
+}
+
+let num1 = 1
+let num2 = 5
+let num3 = 10
+let total = num1 + num2 + num3 
+console.log(total)
