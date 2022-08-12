9e51391ccee87d3002f796d99e36246c555d7308
wailinnkyaw03 <102975967+wailinnkyaw03@users.noreply.github.com>
2022-08-12 19:22:31 +0630

    a

--- converter/main.js
+++ converter/main.js
@@ -1,10 +1,13 @@
-function sumValue(){
-    let num1, num2, result;
-    num1 = Number(document.myForm.txtnum1.value);
-    num2 = Number(document.myForm.txtnum2.value);
-    unit = document.myForm.unit.value;
-    result = (num1 * num2 ) + unit;
-    // document.getElementById("answer")= result;
-    document.myForm.txtresult.value = result;
-    console.log(result);
-}
+let num1, num2, result;
+num1 = Number(document.myForm.m.value);
+console.log(num1);
+num2 = Number(document.myForm.ft.value);
+console.log(num2);
+// unit = document.myForm.unit.value;
+// console.log(unit);
+// result = (num1 * num2 ) + unit;
+// document.getElementById("answer")= result;
+// document.myForm.txtresult.value = result;
+// console.log(result);
+document.myForm.m.value = num2/3.281;
+document.myForm.ft.value = num1*3.281;
