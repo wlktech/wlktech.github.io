3270f2ed08af36a9cb34d2c5040c7f5bbfe486f1
wailinnkyaw03 <wailinnkyaw03@gmail.com>
2022-11-05 00:18:25 +0630

    a

---
+++ heinlearn/javascript/array.js
@@ -0,0 +1,10 @@
+let studentList = ['Aung Aung', 'Mg Mg', 'Kyaw Kyaw'];
+console.log(studentList);
+studentList.push('Su Su');
+console.log(studentList);
+studentList.unshift('Linn Linn');
+console.log(studentList);
+studentList.pop()
+console.log(studentList);
+studentList.sort()
+console.log(studentList);
