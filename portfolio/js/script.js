e27348cba7122e7b1b082e15cffb2fff964ddfed
wailinnkyaw03 <102975967+wailinnkyaw03@users.noreply.github.com>
2022-11-16 22:44:59 +0630

    a

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -4,3 +4,21 @@ var typed = new Typed(".auto-type", {
     backSpeed:150,
     loop:true
 })
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
