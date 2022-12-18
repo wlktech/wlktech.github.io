ac28627dddb50858eae02cd83475afef1e36ad13
wailinnkyaw03 <102975967+wailinnkyaw03@users.noreply.github.com>
2022-12-18 10:07:05 +0630

    a

--- heinlearn/Bootstrap/homework/onlineshop/js/script.js
+++ heinlearn/Bootstrap/homework/onlineshop/js/script.js
@@ -1,17 +1,53 @@
 $(document).ready(function () {
     var navbar = $('#navbar');
     $(window).scroll(function(){
-        if($(window).scrollTop() <=100){
+        if($(window).scrollTop() <=80){
             navbar.slideDown('slow');
         }else{
             navbar.slideUp('slow');
         }
     })
 
-    $('.autoplay').slick({
+    // $('.autoplay').slick({
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 2000,
+    //   });
+    $('.responsive').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        prevArrow: '<i class="fas fa-angle-left left_arrow">',
+        nextArrow: '<i class="fas fa-angle-right right_arrow">',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
 });
