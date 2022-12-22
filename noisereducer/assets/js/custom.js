d895de924b03b560561a1779206f3c0fc028ae9a
wailinnkyaw03 <102975967+wailinnkyaw03@users.noreply.github.com>
2022-12-22 12:53:52 +0630

    a

---
+++ noisereducer/assets/js/custom.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('.responsive').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<i class="fas fa-angle-left left_arrow">',
+        nextArrow: '<i class="fas fa-angle-right right_arrow">',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+});
