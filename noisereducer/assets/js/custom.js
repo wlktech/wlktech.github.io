4514ac3f7cbb2721d91be45b788636cecaf7ed7c
wailinnkyaw03 <102975967+wailinnkyaw03@users.noreply.github.com>
2022-12-23 01:39:06 +0630

    a

--- noisereducer/assets/js/custom.js
+++ noisereducer/assets/js/custom.js
@@ -35,6 +35,67 @@ $(document).ready(function () {
           }
         ]
       });
+
+      $('#all').click(function(){
+        $('.foam').show('slow');
+        $('.fabric').show('slow');
+        $('.polyester').show('slow');
+        $('.diffusor').show('slow');
+
+        $('#all').addClass('active');
+        $('#foam').removeClass('active');
+        $('#fabric').removeClass('active');
+        $('#polyester').removeClass('active');
+        $('#diffusor').removeClass('active');
+    })
+    $('#foam').click(function(){
+        $('.foam').show('slow');
+        $('.fabric').hide('slow');
+        $('.polyester').hide('slow');
+        $('.diffusor').hide('slow');
+
+        $('#all').removeClass('active');
+        $('#foam').addClass('active');
+        $('#fabric').removeClass('active');
+        $('#polyester').removeClass('active');
+        $('#diffusor').removeClass('active');
+    })
+    $('#fabric').click(function(){
+        $('.foam').hide('slow');
+        $('.fabric').show('slow');
+        $('.polyester').hide('slow');
+        $('.diffusor').hide('slow');
+
+        $('#all').removeClass('active');
+        $('#foam').removeClass('active');
+        $('#fabric').addClass('active');
+        $('#polyester').removeClass('active');
+        $('#diffusor').removeClass('active');
+    })
+    $('#polyester').click(function(){
+        $('.foam').hide('slow');
+        $('.fabric').hide('slow');
+        $('.polyester').show('slow');
+        $('.diffusor').hide('slow');
+
+        $('#all').removeClass('active');
+        $('#foam').removeClass('active');
+        $('#fabric').removeClass('active');
+        $('#polyester').addClass('active');
+        $('#diffusor').removeClass('active');
+    })
+    $('#diffusor').click(function(){
+        $('.foam').hide('slow');
+        $('.fabric').hide('slow');
+        $('.polyester').hide('slow');
+        $('.diffusor').show('slow');
+
+        $('#all').removeClass('active');
+        $('#foam').removeClass('active');
+        $('#fabric').removeClass('active');
+        $('#polyester').removeClass('active');
+        $('#diffusor').addClass('active');
+    })
     
 
 });
