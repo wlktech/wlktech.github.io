e0c9de6b60241b1cb759dbf9b383d313b1963316
wailinnkyaw03 <wailinnkyaw03@gmail.com>
2022-12-31 13:33:11 +0630

    a

--- reactProject/project-frontend/app-frontend/src/frontend/Frontends.js
+++ reactProject/project-frontend/app-frontend/src/frontend/Frontends.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Navigation from "./Navigation";
-
+import Header from "./Header";
+import Service from "./Service";
 
 
 function Frontends(props){
     return(
         <>
         <Navigation></Navigation>
-        
+        <Header></Header>
+        <Service></Service>
         </>
     )
 }
--- reactProject/project-frontend/app-frontend/src/frontend/Navigation.js
+++ reactProject/project-frontend/app-frontend/src/frontend/Navigation.js
@@ -2,28 +2,28 @@ import React from "react";
 
 function Navigation(props){
     return(
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark" id="navbar">
             <div className="container">
-                <a className="navbar-brand" href="javascript:void(0)">WAI LINN KYAW</a>
+                <a className="navbar-brand" href="#">SHOPPING</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="mynavbar">
                 <ul className="navbar-nav ms-auto">
                     <li className="nav-item">
-                    <a className="nav-link" href="javascript:void(0)">HOME</a>
+                    <a className="nav-link active" href="#">HOME</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="javascript:void(0)">ABOUT</a>
+                    <a className="nav-link active" href="#">ABOUT</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="javascript:void(0)">SKILLS</a>
+                    <a className="nav-link active" href="#">SHOPS</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="javascript:void(0)">PORTFOLIO</a>
+                    <a className="nav-link active" href="#">PROMOTIONS</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="javascript:void(0)">CONTACT</a>
+                    <a className="nav-link active" href="#">CONTACT</a>
                     </li>
                 </ul>
                 </div>
