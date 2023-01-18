a4a0d87133a745d7bc3587dfc923fc1a5d2ee6ab
wailinnkyaw03 <102975967+wailinnkyaw03@users.noreply.github.com>
2023-01-18 08:55:12 +0630

    a

--- wlktech/assets/js/custom.js
+++ wlktech/assets/js/custom.js
@@ -1,121 +1,62 @@
-const date = new Date();
-let month = date.getMonth();
-let day = date.getDate();
-let year = date.getFullYear();
-    switch (month) {
-        case 0:
-          month = "JAN";
-          break;
-        case 1:
-          month = "FEB";
-          break;
-        case 2:
-           month = "MAR";
-          break;
-        case 3:
-          month = "APR";
-          break;
-        case 4:
-          month = "MAY";
-          break;
-        case 5:
-          month = "JUNE";
-          break;
-        case 6:
-          month = "JUL";
-          break;
-        case 7:
-          month = "AUG";
-          break;
-        case 8:
-          month = "SEP";
-          break;
-        case 9:
-          month = "OCT";
-          break;
-        case 10:
-          month = "NOV";
-          break;
-        case 11:
-          month = "DEC";
-          break;
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
       }
-// console.log(day, "/", month, "/", year);
-// $("#checkIn").val(year, "/", month, "/", day);
+    }
 
-// booking
-const guestBtn = document.querySelector("#guests-input-btn"),
-	guestOptions = document.querySelector("#guests-input-options"),
-	adultsSubsBtn = document.querySelector("#adults-subs-btn"),
-	adultsAddBtn = document.querySelector("#adults-add-btn"),
-	childrenSubsBtn = document.querySelector("#children-subs-btn"),
-	childrenAddBtn = document.querySelector("#children-add-btn"),
-	adultsCountEl = document.querySelector("#guests-count-adults"),
-	childrenCountEl = document.querySelector("#guests-count-children");
-let maxNumGuests = 15,
-	isGuestInputOpen = false,
-	adultsCount = 1,
-	childrenCount = 0;
-updateValues();
-guestBtn.addEventListener('click', function (e) {
-	if (isGuestInputOpen) {
-		guestBtn.classList.remove("open");
-		guestOptions.classList.remove("open");
-	} else {
-		guestBtn.classList.add("open");
-		guestOptions.classList.add("open");
-	}
-	isGuestInputOpen = isGuestInputOpen ? false : true;
-	e.preventDefault();
-});
-adultsAddBtn.addEventListener('click', function () {
-	adultsCount = addValues(adultsCount);
-	updateValues();
-});
-adultsSubsBtn.addEventListener('click', function () {
-	adultsCount = substractValues(adultsCount, 1);
-	updateValues();
-});
-childrenAddBtn.addEventListener('click', function () {
-	childrenCount = addValues(childrenCount);
-	updateValues();
-});
-childrenSubsBtn.addEventListener('click', function () {
-	childrenCount = substractValues(childrenCount, 0);
-	updateValues();
-});
+	var zoomin = document.querySelectorAll(".zoomin");
+          
+    for (var i = 0; i < zoomin.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = zoomin[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+          
+    if (elementTop < windowHeight - elementVisible) {
+        zoomin[i].classList.add("Zoomin");
+    } else {
+        zoomin[i].classList.remove("Zoomin");
+        }
+    }
 
-function calcTotalGuests() {
-	return adultsCount + childrenCount;
-}
+	var leftin = document.querySelectorAll(".leftin");
+          
+    for (var i = 0; i < leftin.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = leftin[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+          
+    	if (elementTop < windowHeight - elementVisible) {
+        leftin[i].classList.add("Leftin");
+        } else {
+        leftin[i].classList.remove("Leftin");
+        }
+    }
+          
 
-function addValues(count) {
-	return (calcTotalGuests() < maxNumGuests) ? count + 1 : count;
-}
+    var rightin = document.querySelectorAll(".rightin");
+          
+    for (var i = 0; i < rightin.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = rightin[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+          
+        if (elementTop < windowHeight - elementVisible) {
+        	rightin[i].classList.add("Rightin");
+        } else {
+            rightin[i].classList.remove("Rightin");
+        }
+    }
 
-function substractValues(count, min) {
-	return (count > min) ? count - 1 : count;
-}
 
-function updateValues() {
-	let btnText = `${adultsCount} Adults`;
-	btnText += (childrenCount > 0) ? `, ${childrenCount} Children` : '';
-	guestBtn.innerHTML = btnText;
-	adultsCountEl.innerHTML = adultsCount;
-	childrenCountEl.innerHTML = childrenCount;
-	if (adultsCount == 1) {
-		adultsSubsBtn.classList.add("disabled");
-	} else {
-		adultsSubsBtn.classList.remove("disabled");
-	} if (childrenCount == 0) {
-		childrenSubsBtn.classList.add("disabled");
-	} else {
-		childrenSubsBtn.classList.remove("disabled");
-	} if (calcTotalGuests() == maxNumGuests) {
-		adultsAddBtn.classList.add("disabled");
-		childrenAddBtn.classList.add("disabled");
-	} else {
-		adultsAddBtn.classList.remove("disabled");
-		childrenAddBtn.classList.remove("disabled");
-	}
-}
+  }
+  
+  window.addEventListener("scroll", reveal);
